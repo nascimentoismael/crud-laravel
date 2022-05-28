bdce08e09a158c3798b7246bff430667046658b5
Ismael Nascimento <ismaelazevedonascimento@gmail.com>
2022-05-28 20:30:58 -0300

    crud updates on model, controller and routes

--- crud-medico/routes/web.php
+++ crud-medico/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/medico', function () {
+    return view('medico');
+});
+
+Route::resources('/medico', 'MedicoController');
